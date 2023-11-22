1b6f37b55565d207ba6bbf1bb6fca50952853a4e
mkshie <87657174+mkshie@users.noreply.github.com>
2023-11-22 20:16:03 +0900

    Update userController.js

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -76,7 +76,7 @@ exports.signin = async function (req, res) {
 exports.postUsers = async function (req, res) {
   const { email, password, userName, birth, phoneNum, sex} = req.body;
   // email checking and print error message
-
+  console.log("test2")
   // 빈 값 체크
   if (!email) return res.send(response(baseResponse.SIGNUP_EMAIL_EMPTY));
   // 길이 체크
